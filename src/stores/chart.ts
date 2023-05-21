9f0188c2d25c28394af80baa4dee656f53a499c5
Benjamin Lumbye <benjamin@lumbye.net>
2023-05-21 21:04:43 +0200

    Fixed type issue

--- src/stores/chart.ts
+++ src/stores/chart.ts
@@ -4,7 +4,7 @@ export const useChartStore = defineStore('chart', () => {
   // State
   const chartData = ref([{ id: "A", value: 28 }, { id: "B", value: 55 }, { id: "C", value: 43 }, { id: "D", value: 91 }, { id: "E", value: 81 }, { id: "F", value: 53 }, { id: "G", value: 19 }, { id: "H", value: 87 }]);
   const headers = { id: "ID", value: "Value" };
-  const types = { id: "string", value: "number" };
+  const types = { id: "string", value: "number" } satisfies Record<string, "string" | "number" | "boolean">;
 
   return { chartData, headers, types };
 });
